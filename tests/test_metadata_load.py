cd31c2f3cba138a2013d1a2b059753a7ba71edc0
Mirek Simek <miroslav.simek@gmail.com>
2025-07-13 18:38:19 +0200

    Tests

--- tests/test_metadata_load.py
+++ tests/test_metadata_load.py
@@ -8,21 +8,23 @@ from oarepo_model.presets.records_resources import records_presets
 def test_metadata_load_from_dict(
     app,
 ):
-    model(
+    m = model(
         name="metadata_load_test",
         version="1.0.0",
         presets=[records_presets, drafts_records_presets],
-        types={
-            "RecordMetadata": {"properties": {"title": {"type": "TitleType"}}},
-            "TitleType": {
-                "type": "fulltext+keyword",
-            },
-        },
+        types=[
+            {
+                "RecordMetadata": {"properties": {"title": {"type": "TitleType"}}},
+                "TitleType": {
+                    "type": "fulltext+keyword",
+                },
+            }
+        ],
         metadata_type="RecordMetadata",
     )
 
-    assert issubclass(model.RecordSchema, Schema)
-    assert model.RecordSchema().load(
+    assert issubclass(m.RecordSchema, Schema)
+    assert m.RecordSchema().load(
         {
             "metadata": {
                 "title": "Test Title",
