688b66a90b830aa48124ac8cb7dc10c4ce8211f9
Mirek Simek <miroslav.simek@gmail.com>
2025-07-19 13:17:27 +0200

    tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PIP_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
-export UV_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
+# export PIP_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
+# export UV_EXTRA_INDEX_URL=https://gitlab.cesnet.cz/api/v4/projects/1408/packages/pypi/simple
 
-PYTHON=python3
+PYTHON=python3.13
 
 set -e
 
@@ -19,6 +19,6 @@ $PYTHON -m venv $VENV
 source $VENV/bin/activate
 
 pip install -U setuptools pip wheel
-pip install -e '.[dev,oarepo13,tests]'
+pip install -e ".[dev,oarepo${OAREPO_VERSION},tests]"
 
 pytest tests
