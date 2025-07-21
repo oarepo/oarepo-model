846d10ffb2888fa8a68de202e5dd44809c5a0b85
Mirek Simek <miroslav.simek@gmail.com>
2025-07-21 07:20:55 +0200

    fix: better formulation of default fields

--- src/oarepo_model/datatypes/vocabularies.py
+++ src/oarepo_model/datatypes/vocabularies.py
@@ -37,89 +37,19 @@ class VocabularyDataType(PIDRelation):
             else:
                 raise ValueError(f"Invalid key type: {type(key)}")
 
+        # if 'id' is not in keys, add it as a keyword field
         if "id" not in known_keys:
             keys.append({"id": {"type": "keyword"}})
-        match element["vocabulary-type"]:
-            case "affiliations" | "funders":
-                if "identifiers" not in known_keys:
-                    keys.append(
-                        {
-                            "identifiers": {
-                                "type": "array",
-                                "items": {
-                                    "type": "object",
-                                    "properties": {
-                                        "scheme": {"type": "keyword"},
-                                        "identifier": {"type": "keyword"},
-                                    },
-                                },
-                            }
-                        }
-                    )
-                if "name" not in known_keys:
-                    keys.append({"name": {"type": "keyword"}})
-            case "awards":
-                if "title" not in known_keys:
-                    keys.append({"title": {"type": "i18ndict"}})
-                if "number" not in known_keys:
-                    keys.append({"number": {"type": "keyword"}})
-                if "identifiers" not in known_keys:
-                    keys.append(
-                        {
-                            "identifiers": {
-                                "type": "array",
-                                "items": {
-                                    "type": "object",
-                                    "properties": {
-                                        "scheme": {"type": "keyword"},
-                                        "identifier": {"type": "keyword"},
-                                    },
-                                },
-                            }
-                        }
-                    )
-                if "acronym" not in known_keys:
-                    keys.append({"acronym": {"type": "keyword"}})
-                if "program" not in known_keys:
-                    keys.append({"program": {"type": "keyword"}})
-                if "subjects" not in known_keys:
-                    keys.append(
-                        {
-                            "subjects": {
-                                "type": "array",
-                                "items": {
-                                    "type": "vocabulary",
-                                    "vocabulary-type": "subjects",
-                                },
-                            }
-                        }
-                    )
-                if "organizations" not in known_keys:
-                    keys.append(
-                        {
-                            "organizations": {
-                                "type": "array",
-                                "items": {
-                                    "type": "object",
-                                    "properties": {
-                                        "scheme": {"type": "keyword"},
-                                        "id": {"type": "keyword"},
-                                        "organization": {"type": "keyword"},
-                                    },
-                                },
-                            }
-                        }
-                    )
-            case "subjects":
-                if "subject" not in known_keys:
-                    keys.append({"subject": {"type": "keyword"}})
-                if "scheme" not in known_keys:
-                    keys.append({"scheme": {"type": "keyword"}})
-                if "props" not in known_keys:
-                    keys.append({"props": {"type": "dynamic-object"}})
-            case _generic:
-                if "title" not in known_keys:
-                    keys.append({"title": {"type": "i18ndict"}})
+
+        # add other fields based on the vocabulary type
+        vocabulary_fields = (
+            default_vocabulary_fields_in_relations.get(element["vocabulary-type"])
+            or default_vocabulary_fields_in_relations["*"]
+        )
+        for prop in vocabulary_fields:
+            for key, value in prop.items():
+                if key not in known_keys:
+                    keys.append({key: value})
 
         return super()._get_properties(element)
 
@@ -186,3 +116,80 @@ class VocabularyDataType(PIDRelation):
         self, element: dict[str, Any], path: list[tuple[str, dict[str, Any]]]
     ) -> str | None:
         return super()._cache_key(element, path) or element["vocabulary-type"]
+
+
+default_vocabulary_fields_in_relations: dict[str, list[dict[str, Any]]] = {
+    "affiliations": [
+        {
+            "identifiers": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "scheme": {"type": "keyword"},
+                        "identifier": {"type": "keyword"},
+                    },
+                },
+            }
+        },
+        {"name": {"type": "keyword"}},
+    ],
+    "funders": [
+        {
+            "identifiers": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "scheme": {"type": "keyword"},
+                        "identifier": {"type": "keyword"},
+                    },
+                },
+            }
+        },
+        {"name": {"type": "keyword"}},
+    ],
+    "awards": [
+        {"title": {"type": "i18ndict"}},
+        {"number": {"type": "keyword"}},
+        {
+            "identifiers": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "scheme": {"type": "keyword"},
+                        "identifier": {"type": "keyword"},
+                    },
+                },
+            }
+        },
+        {"acronym": {"type": "keyword"}},
+        {"program": {"type": "keyword"}},
+        {
+            "subjects": {
+                "type": "array",
+                "items": {"type": "vocabulary", "vocabulary-type": "subjects"},
+            }
+        },
+        {
+            "organizations": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "scheme": {"type": "keyword"},
+                        "id": {"type": "keyword"},
+                        "organization": {"type": "keyword"},
+                    },
+                },
+            }
+        },
+    ],
+    "subjects": [
+        {"subject": {"type": "keyword"}},
+        {"scheme": {"type": "keyword"}},
+        {"props": {"type": "dynamic-object"}},
+    ],
+    "*": [{"title": {"type": "i18ndict"}}],
+}
