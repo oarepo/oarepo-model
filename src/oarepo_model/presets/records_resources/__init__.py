56a238ff45f7540715257c1430ef323f6bf07791
senyaaa <pogodars@cvut.cz>
2025-07-23 16:00:18 +0200

    response handlers for UI serialization

--- src/oarepo_model/presets/records_resources/__init__.py
+++ src/oarepo_model/presets/records_resources/__init__.py
@@ -35,8 +35,12 @@ from .records.relations import RelationsPreset
 from .records.relations_dumper_ext import RelationsDumperExtPreset
 from .resources.files.file_resource import FileResourcePreset
 from .resources.files.file_resource_config import FileResourceConfigPreset
+from .resources.records.register_ui_json_serializer import (
+    RegisterJSONUISerializerPreset,
+)
 from .resources.records.resource import RecordResourcePreset
 from .resources.records.resource_config import RecordResourceConfigPreset
+from .resources.records.ui_json_serializer import JSONUISerializerPreset
 from .services.files.file_record_service_components import (
     FileRecordServiceComponentsPreset,
 )
@@ -56,6 +60,8 @@ from .services.records.results import (
 from .services.records.search_options import RecordSearchOptionsPreset
 from .services.records.service import RecordServicePreset
 from .services.records.service_config import RecordServiceConfigPreset
+from .services.records.ui_metadata_schema import MetadataUISchemaPreset
+from .services.records.ui_record_schema import RecordUISchemaPreset
 
 records_presets = [
     # record layer
@@ -82,9 +88,13 @@ records_presets = [
     PermissionPolicyPreset,
     RecordSchemaPreset,
     MetadataSchemaPreset,
+    RecordUISchemaPreset,
+    MetadataUISchemaPreset,
     # resource layer
     RecordResourcePreset,
     RecordResourceConfigPreset,
+    JSONUISerializerPreset,
+    RegisterJSONUISerializerPreset,
     # extension
     ExtPreset,
     ProxyPreset,
---
+++ src/oarepo_model/presets/records_resources/resources/records/register_ui_json_serializer.py
@@ -0,0 +1,53 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-model (see http://github.com/oarepo/oarepo-model).
+#
+# oarepo-model is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Generator
+
+from flask_resources import ResponseHandler
+from invenio_records_resources.resources.records.headers import etag_headers
+from werkzeug.local import LocalProxy
+
+from oarepo_model.customizations import (
+    AddToDictionary,
+    Customization,
+)
+from oarepo_model.model import InvenioModel
+from oarepo_model.presets import Preset
+
+if TYPE_CHECKING:
+    from oarepo_model.builder import InvenioModelBuilder
+
+class RegisterJSONUISerializerPreset(Preset):
+    """
+    Preset for registering JSON UI Serializer.
+    """
+
+    depends_on = ["JSONUISerializer"]
+    modifies = ["record_response_handlers"]
+
+    def apply(
+        self,
+        builder: InvenioModelBuilder,
+        model: InvenioModel,
+        dependencies: dict[str, Any],
+    ) -> Generator[Customization, None, None]:
+        
+        # maybe use local proxy instead of dependencies['UIJSONSerializer']()
+        runtime_deps = builder.get_runtime_dependencies()
+        proxy = LocalProxy(lambda:ResponseHandler(
+                    runtime_deps.get('JSONUISerializer')(), headers=etag_headers
+                ))
+        
+        yield AddToDictionary(
+            "record_response_handlers",
+            {
+                "application/vnd.inveniordm.v1+json": proxy
+            },
+        )
