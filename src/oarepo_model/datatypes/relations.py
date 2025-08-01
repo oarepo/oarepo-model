e772743b509855b74b0024c32f5ce17e71a3b069
senyaaa <pogodars@cvut.cz>
2025-08-01 13:01:26 +0200

    temporary fix for tests

--- src/oarepo_model/datatypes/relations.py
+++ src/oarepo_model/datatypes/relations.py
@@ -58,10 +58,13 @@ class PIDRelation(ObjectDataType):
         return ret
 
     @override
+    def create_ui_marshmallow_fields(self, field_name, element):
+        return {}  # TODO: dont know what is supposed to be here yet
+
+    @override
     def create_relations(
         self, element: dict[str, Any], path: list[tuple[str, dict[str, Any]]]
     ) -> list[Customization]:
-
         relation_path = self._relation_path(element, path)
         relation_name = self._relation_name(element, path)
         pid_field = self._pid_field(element, path)
