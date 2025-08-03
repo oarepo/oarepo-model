60ec5717b1825f4b5e513779ea3cec421d7d2b34
Mirek Simek <miroslav.simek@gmail.com>
2025-08-03 20:08:49 +0200

    Composable workflows

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 base_dir="$(dirname "$0")"
 
 if [ ! -f "${base_dir}/.runner.sh" ]; then
-  echo "Downloading .runner.sh from oarepo repository..."
+  echo "Downloading .runner.sh from oarepo repository..." >&2
   curl -o "${base_dir}/.runner.sh" https://raw.githubusercontent.com/oarepo/oarepo/main/tools/runner.sh
   chmod +x "${base_dir}/.runner.sh"
 fi
