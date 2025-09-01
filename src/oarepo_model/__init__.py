88873865af6ba47260234a6c2a2aaee74584da7b
senyaaa <pogodars@cvut.cz>
2025-09-01 10:31:28 +0200

    format

--- src/oarepo_model/__init__.py
+++ src/oarepo_model/__init__.py
@@ -19,4 +19,4 @@ from __future__ import annotations
 from .datatypes.registry import from_json, from_yaml
 
 __version__ = "0.1.0dev5"
-__all__ = ["from_json", "from_yaml", "__version__"]
+__all__ = ["__version__", "from_json", "from_yaml"]
