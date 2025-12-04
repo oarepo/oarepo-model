cf1c719e429fecb3fa2b1c7f08e0df204effef16
Ronald Krist <34395689+SilvyPuzzlewell@users.noreply.github.com>
2025-12-04 14:03:24 +0100

    Service component customization (#77)
    * high level customization for adding record service components
    
    * changed list to sequence in api.model arguments
    
    * fixed copypaste error

--- tests/test_customizations.py
+++ tests/test_customizations.py
@@ -12,16 +12,20 @@ import json
 from unittest.mock import MagicMock
 
 import pytest
+from invenio_records_resources.services.records.components import ServiceComponent
 
+from oarepo_model.api import model
 from oarepo_model.builder import InvenioModelBuilder
 from oarepo_model.customizations import (
     AddJSONFile,
     AddModule,
+    AddServiceComponent,
     AddToDictionary,
     AddToList,
     AddToModule,
     PatchIndexSettings,
 )
+from oarepo_model.presets.records_resources import records_resources_preset
 
 
 def test_add_to_dictionary():
@@ -116,3 +120,23 @@ def test_index_customizations():
             "f": "abc",
         }
     }
+
+
+class TestServiceComponent(ServiceComponent):
+    """Test service component."""
+
+
+def test_add_service_component():
+    m = model(
+        name="test_add_service_component",
+        version="1.0.0",
+        presets=[
+            records_resources_preset,
+        ],
+        customizations=[
+            AddServiceComponent(TestServiceComponent),
+        ],
+    )
+
+    # ideally check whether the component actually ends in the service components list after app init
+    assert len([c for c in m.record_service_components if issubclass(c, TestServiceComponent)]) == 1
