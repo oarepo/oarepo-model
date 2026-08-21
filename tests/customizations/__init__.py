aa861e37b439cf3e564070e54d6aadb279d030fb
Mirek Simek <miroslav.simek@cesnet.cz>
2026-08-21 19:31:08 +0200

    Add copyright headers to test package __init__.py files
    tests/api_tests, tests/customizations and tests/datatypes were left
    without the standard header when their (previously empty) __init__.py
    files were added.

---
+++ tests/customizations/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2026 University of West Bohemia
+#
+# This file is a part of oarepo-model (see https://github.com/oarepo/oarepo-model).
+#
+# oarepo-model is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Customizations tests."""
+
+from __future__ import annotations
